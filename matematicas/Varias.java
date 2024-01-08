3e6c8fda23996e7775c55b98e454472d58cd25d0
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-08 10:37:38 +0100

    commit

--- matematicas/Varias.java
+++ matematicas/Varias.java
@@ -16,6 +16,40 @@ public class Varias {
         }
     }
 
+    /**
+     *  Devuelve verdadero si el número que se pasa como parámetro
+     *  es primo y falso en caso contrario.
+     * @param n
+     * @return esPrimo
+     */
+    public static boolean esPrimo(int n){
+        boolean esPrimo = true;
+        for (int i = 2; i < n/2+1; i++) {
+            if(n%i==0){
+                esPrimo = false;
+            }
+        }
+
+        return esPrimo;
+    }
+
     
+    /**
+     *  Devuelve el menor primo que es mayor al número que
+        se pasa como parámetro
+     * @param n
+     * @return
+     */
+    public static int siguientePrimo(int n){
+        boolean salir = false;
+        do {
+            n++;
+            if(esPrimo(n)){
+                salir = true;
+            }
+        } while (salir==false);
+
+        return n;
+    }
 
 }
