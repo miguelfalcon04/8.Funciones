ec3c623604d35779c60f77c04e8952df875a2b18
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-17 10:29:50 +0100

    commit

--- array/ArrayBi.java
+++ array/ArrayBi.java
@@ -63,5 +63,31 @@ public class ArrayBi {
             && (Array.maximoArrayInt(columna) == x[i][j]));
     }
 
+    public static int[] diagonal(int x[][], int fila, int columna, String direccion) {
+    
+        int elementos = 0, i, j;
+        int[] diagonalAux = new int [1000];
+        
+        for (i = 0; i < x.length; i++) {
+            for (j = 0; j < x[0].length; j++) {
+                if ( (((columna - j) == (fila - i)) && (direccion.equals("nose")))
+                    || (((columna - j) == (i - fila)) && (direccion.equals("neso"))) ) {
+                    diagonalAux[elementos++] = x[i][j];
+                }
+            }
+        }
+    
+        int[] diagonal = new int[elementos];
+        for (j = 0; j < elementos; j++) {
+            diagonal[j] = diagonalAux[j];
+        }
+    
+        return diagonal;
+    }
+
+    public static int nEsimo(int[][] n, int posicion){
+        int num = 0;
+        int cont = 0;
+        boolean captado = false;
 
 }
