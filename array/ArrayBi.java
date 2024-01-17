23a896e58351edf870963281ed04dfad0561af9e
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-01-17 10:28:23 +0100

    commit

--- array/ArrayBi.java
+++ array/ArrayBi.java
@@ -14,6 +14,26 @@ public class ArrayBi {
         return array;
     }
 
-    
+    public static int[] filaDeArrayBiInt(int[][] array, int fila){
+        int[] f = new int[array[0].length];
+
+        for (int j = 0; j < f.length; j++) {
+            f[j] = array[fila][j];
+        }
+
+        return f;
+    }
+
+    public static int[] columnaDeArrayBiInt(int[][] array, int columna){
+        int[] c = new int[array.length];
+
+        for (int i = 0; i < c.length; i++) {
+            c[i] = array[i][columna];
+        }
+
+        return c;
+    }
+
+
 
 }
